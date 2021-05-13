74b352561958046c8f0ef02d78c97534d177dd66
CaiuPelado <caiomorais99@outlook.com>
2021-05-13 20:47:51 -0300

    insert gastos e dividas

--- Small-Financial/src/telassmallfinancial/GastosInsertController.java
+++ Small-Financial/src/telassmallfinancial/GastosInsertController.java
@@ -5,7 +5,10 @@
  */
 package telassmallfinancial;
 
+import DAO.DAOGastos;
+import MODEL.Gastos;
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -24,8 +28,8 @@ public class GastosInsertController implements Initializable {
     /**
      * Initializes the controller class.
      */
-     @FXML
-    private ComboBox<?> cbCategoria;
+    @FXML
+    private TextField cbCategoria;
 
     @FXML
     private TextField txtValor;
@@ -41,10 +45,29 @@ public class GastosInsertController implements Initializable {
 
     @FXML
     private Button btnVoltar;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    public void inserir() {
+        try {
+            DAOGastos gastos = new DAOGastos();
+            Gastos gasto = new Gastos();
+            gasto.setCategoria(cbCategoria.getText());
+            Date date = Date.valueOf(txtData.getText());
+            gasto.setDataGasto(date);
+            gasto.setIdUsuario(1);
+            gasto.setObservacao(txtObservacoes.getText());
+            gasto.setPreco(Double.parseDouble(txtValor.getText()));
+            gastos.inserir(gasto);
+            JOptionPane.showConfirmDialog(null, "Cadastrado com sucesso!", "Alerta!", JOptionPane.DEFAULT_OPTION);
+        }
+        catch(Exception e){
+            JOptionPane.showConfirmDialog(null, e.toString(), "Ops, algo deu errado", JOptionPane.DEFAULT_OPTION);
+            System.out.println(e);
+        }
+    }
 }
