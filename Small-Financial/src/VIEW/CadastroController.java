99b9550438d5a7ddd03243715ba505a228fd9f2d
xJoTaErRex <74562321+xJoTaErRex@users.noreply.github.com>
2021-05-18 23:32:33 -0300

    Commit Padronização

---
+++ Small-Financial/src/VIEW/CadastroController.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIEW;
+
+import DAO.DAOUsuario;
+import MODEL.Usuario;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Caio
+ */
+public class CadastroController implements Initializable {
+
+    private BorderPane rootLayout;
+    @FXML
+    private Button btnVoltar;
+    @FXML
+    private Button btnSalvar;
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private PasswordField txtSenha;
+    @FXML
+    private PasswordField txtConfirmarSenha;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void Voltar(ActionEvent event) throws IOException {
+        Parent voltar = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene voltarScene = new Scene(voltar);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(voltarScene);
+        window.show();
+    }
+
+    @FXML
+    private void Cadastrar(ActionEvent event) throws IOException {
+        DAOUsuario dao = new DAOUsuario();
+        String nome = txtNome.getText();
+        String email = txtEmail.getText();
+        String senha = txtSenha.getText();
+        String confirmacaoSenha = txtConfirmarSenha.getText();
+
+        if (!nome.equals("") && !email.equals("") && !senha.equals("") && !confirmacaoSenha.equals("")) {
+            if (senha.equals(confirmacaoSenha)) {
+                Usuario u = new Usuario();
+                u.setNome(nome);
+                u.setEmail(email);
+                u.setSenha(senha);
+                dao.inserir(u);
+                Alert alerta = new Alert(Alert.AlertType.CONFIRMATION, "Cadastrado com sucesso!", ButtonType.OK);
+                alerta.show();
+                
+                //voltar para login
+                Parent voltar = FXMLLoader.load(getClass().getResource("Login.fxml"));
+                Scene voltarScene = new Scene(voltar);
+                Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                window.setScene(voltarScene);
+                window.show();
+                
+            } else {
+                Alert alerta = new Alert(Alert.AlertType.WARNING, "Senhas não coincidem", ButtonType.OK);
+                alerta.show();
+            }
+        } else {
+            Alert alerta = new Alert(Alert.AlertType.WARNING, "Todos os campos precisam estar preenchidos", ButtonType.OK);
+            alerta.show();
+        }
+    }
+
+}
---
+++ Small-Financial/src/VIEW/EducacaoController.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIEW;
+
+import DAO.DAOCurso_Online;
+import DAO.DAOVideo;
+import MODEL.Curso_Online;
+import MODEL.Video;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Dell
+ */
+public class EducacaoController implements Initializable {
+
+    @FXML
+    private TableView<Video> tvVideos;
+    @FXML
+    private TableColumn<Video, String> tcDescricaoVideo;
+    @FXML
+    private TableColumn<Video, String> tcLinkVideo;
+
+    @FXML
+    private Button btnVoltar;
+    
+    @FXML
+    private Button btnSiglas;
+    
+    @FXML
+    public TableView<Curso_Online> tvCursos;
+    @FXML
+    public TableColumn<Curso_Online, String> tcCurso;
+    @FXML
+    public TableColumn<Curso_Online, String> tcLink;
+    @FXML
+    public TableColumn<Curso_Online, Date> tcPrazo;
+    @FXML
+    private TableView<?> tvGanhos;
+    @FXML
+    private TableColumn<?, ?> tcCategoria;
+    @FXML
+    private TableColumn<?, ?> tcPreco;
+    @FXML
+    private TableColumn<?, ?> tcData;
+    @FXML
+    private Button btnInserirCurso;
+    @FXML
+    private Button btnMenuVideo;
+    @FXML
+    private Button btnMenuPublicacoes;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        //LISTAR CURSO
+        tcCurso.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tcLink.setCellValueFactory(new PropertyValueFactory<>("link"));
+        tcPrazo.setCellValueFactory(new PropertyValueFactory<>("dataLimite"));
+       
+        DAOCurso_Online daoCurso = new DAOCurso_Online();
+        ObservableList<Curso_Online> curso = FXCollections.observableArrayList(daoCurso.consultar());
+        tvCursos.setItems(curso);
+        
+        //LISTAR VÍDEO
+        tcDescricaoVideo.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+        tcLinkVideo.setCellValueFactory(new PropertyValueFactory<>("link"));
+       
+       
+        DAOVideo daoVideo = new DAOVideo();
+        ObservableList<Video> video = FXCollections.observableArrayList(daoVideo.consultar());
+        tvVideos.setItems(video);
+        // TODO
+    }    
+
+    @FXML
+    private void Voltar(ActionEvent event) throws IOException {
+        Parent voltar = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene voltarScene = new Scene(voltar);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(voltarScene);
+        window.show();
+    }
+    @FXML
+    private void MenuCursos(ActionEvent event) throws IOException {
+        Parent insere = FXMLLoader.load(getClass().getResource("MenuCursos.fxml"));
+        Scene insereScene = new Scene(insere);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(insereScene);
+        window.show();
+    }
+    
+    @FXML
+    private void MenuVideos(ActionEvent event) throws IOException {
+        Parent insere = FXMLLoader.load(getClass().getResource("MenuVideos.fxml"));
+        Scene insereScene = new Scene(insere);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(insereScene);
+        window.show();
+    }
+
+    @FXML
+    private void MenuPublicacao(ActionEvent event) throws IOException {
+        Parent insere = FXMLLoader.load(getClass().getResource("MenuPublicacoes.fxml"));
+        Scene insereScene = new Scene(insere);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(insereScene);
+        window.show();
+    }
+
+    
+    @FXML
+    private void siglas(ActionEvent event) throws IOException {
+        Parent telaSiglas = FXMLLoader.load(getClass().getResource("Siglas_e_Nomeclaturas.fxml"));
+        Scene insereVideoScene = new Scene(telaSiglas);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(insereVideoScene);
+        window.show();
+        window.show();
+    }
+}
