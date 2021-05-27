a320cd5fd635370b1136ecc6f0f02737aad5c89e
João Guilherme Cabral <joaoguilherme.sbc@gmail.com>
2021-05-27 00:58:26 -0300

    Inserindo Controle de Acesso ao Menu Educação e telas de Visualização de Curso e Vídeo

--- Small-Financial/src/VIEW/EducacaoController.java
+++ Small-Financial/src/VIEW/EducacaoController.java
@@ -6,13 +6,19 @@
 package VIEW;
 
 import DAO.DAOCurso_Online;
+import DAO.DAOUsuario;
 import DAO.DAOVideo;
 import MODEL.Curso_Online;
+import MODEL.Metas;
 import MODEL.Video;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,8 +30,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
@@ -56,26 +65,50 @@ public class EducacaoController implements Initializable {
     public TableColumn<Curso_Online, String> tcLink;
     @FXML
     public TableColumn<Curso_Online, Date> tcPrazo;
+    
+    
+    
+      @FXML
+    public Button btnMenuPublicacao;
+
     @FXML
-    private TableView<?> tvGanhos;
+    public Button btnMenuCurso;
+
     @FXML
-    private TableColumn<?, ?> tcCategoria;
-    @FXML
-    private TableColumn<?, ?> tcPreco;
-    @FXML
-    private TableColumn<?, ?> tcData;
-    @FXML
-    private Button btnInserirCurso;
-    @FXML
-    private Button btnMenuVideo;
-    @FXML
-    private Button btnMenuPublicacoes;
+    public Button btnMenuVideo;
+
+    
+    public static Curso_Online cursoSelecionado;
+    public static Video videoSelecionado;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        //VERIFICAR SE É ADM OU USUÁRIO
+        
+        DAOUsuario daoUser = new DAOUsuario();
+        if(daoUser.IdNome().getVerif_ADM() == false)
+        {
+            btnMenuPublicacao.setVisible(false);
+            btnMenuCurso.setVisible(false);
+            btnMenuVideo.setVisible(false);
+        
+        }
+        if(daoUser.IdNome().getVerif_ADM() == true)
+        {
+            btnMenuPublicacao.setDisable(false);
+            btnMenuCurso.setDisable(false);
+            btnMenuVideo.setDisable(false);
+            btnMenuPublicacao.setVisible(true);
+            btnMenuCurso.setVisible(true);
+            btnMenuVideo.setVisible(true);
+        
+        }
+        
         
         //LISTAR CURSO
         tcCurso.setCellValueFactory(new PropertyValueFactory<>("nome"));
@@ -94,7 +127,55 @@ public class EducacaoController implements Initializable {
         DAOVideo daoVideo = new DAOVideo();
         ObservableList<Video> video = FXCollections.observableArrayList(daoVideo.consultar());
         tvVideos.setItems(video);
-        // TODO
+        
+        
+        //AÇÃO CLICANDO NO VIDEO
+        
+        tvVideos.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                videoSelecionado = (Video) newValue;
+            }
+        });
+        
+        tvVideos.setRowFactory(tv -> {
+            TableRow<Video> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    try {
+                        chamarTelaVisualizacaoVideo(event);
+                    } catch (IOException ex) {
+                        Logger.getLogger(EducacaoController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+            return row;
+        });
+        
+        
+        //AÇÃO CLICANDO NO CURSO
+        tvCursos.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                cursoSelecionado = (Curso_Online) newValue;
+            }
+        });
+        
+        tvCursos.setRowFactory(tv -> {
+            TableRow<Curso_Online> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    try {
+                        chamarTelaVisualizacao(event);
+                    } catch (IOException ex) {
+                        Logger.getLogger(EducacaoController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+            return row;
+        });
+        
+        
     }    
 
     @FXML
@@ -141,4 +222,27 @@ public class EducacaoController implements Initializable {
         window.setScene(insereVideoScene);
         window.centerOnScreen();
     }
-}
+    
+    @FXML
+    private void chamarTelaVisualizacao(MouseEvent event) throws IOException {
+        Parent inserir = FXMLLoader.load(getClass().getResource("VisualizarCurso_Online.fxml"));
+        Scene inserirScene = new Scene(inserir);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(inserirScene);
+        window.centerOnScreen();
+    }
+    
+    @FXML
+    private void chamarTelaVisualizacaoVideo(MouseEvent event) throws IOException {
+        Parent inserir = FXMLLoader.load(getClass().getResource("VisualizarVideo.fxml"));
+        Scene inserirScene = new Scene(inserir);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(inserirScene);
+        window.centerOnScreen();
+    }
+
+   
+    }
+    
+    
+
